88bcc1b7fd3cc2913a29772fb2bc73a277915e27
TheHeatcliff <nicolas.hernan.gonzalez@gmail.com>
2013-10-11 11:55:29 -0300

    adapted for eclipse editor

--- replaceTabs.sh
+++ replaceTabs.sh
@@ -3,6 +3,6 @@ PROYECT_ROOT_FOLDER=/c/Users/n.gonzalez/workspace/PayStadistics
 cd $PROYECT_ROOT_FOLDER
 for i in `find . -name *.java`
 do
-	sed -i 's/\t/    /g' $i
+	sed -i 's/\t/        /g' $i
 done
 	
